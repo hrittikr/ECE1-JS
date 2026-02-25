40af8ee879a3a43400c269fa6eeed6dfe9c9fa66
Hrittik Raushan <hrittikraushan9939@gmail.com>
2026-02-25 10:09:38 +0530

    web design lab 25.02.2026

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 // console.log("Hello World!");
 // console.log("This is a JavaScript file.");
 
-var num =10;
-console.log(num);
-console.log(typeof(num));
-num =false;
-console.log(num);
-console.log(typeof(num));
-var name ="hrittik";
-console.log(typeof(name));
-const PI = 3.14;
-console.log(PI);
-console.log(typeof(PI));
-var a = 5;
-var b = 10;
-var sum = a + b;
-console.log("The sum of a and b is: " + sum);
+// var num =10;
+// console.log(num);
+// console.log(typeof(num));
+// num =false;
+// console.log(num);
+// console.log(typeof(num));
+// var name ="hrittik";
+// console.log(typeof(name));
+// const PI = 3.14;
+// console.log(PI);
+// console.log(typeof(PI));
+// var a = 5;
+// var b = 10;
+// var sum = a + b;
+// console.log("The sum of a and b is: " + sum);
+
+const sym1= Symbol(4);
+console.log(sym1);
+const sym2= Symbol(4);
+console.log(sym2);
+console.log(sym1 == sym2); // Output: false
+if (sym1 == sym2)
+{
+    console.log("true");
+}
+else
+    {
+    console.log("false"); //because each symbol is unique, even if they have the same description.
+}
+

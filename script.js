fb9fe075676b69a8cd242c92c1234de8aa7e742d
Hrittik Raushan <hrittikraushan9939@gmail.com>
2026-02-25 09:50:43 +0530

    web design lab 25.02.2026

--- script.js
+++ script.js
@@ -4,4 +4,15 @@
 var num =10;
 console.log(num);
 console.log(typeof(num));
-
+num =false;
+console.log(num);
+console.log(typeof(num));
+var name ="hrittik";
+console.log(typeof(name));
+const PI = 3.14;
+console.log(PI);
+console.log(typeof(PI));
+var a = 5;
+var b = 10;
+var sum = a + b;
+console.log("The sum of a and b is: " + sum);

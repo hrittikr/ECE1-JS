044c28d2bf727741edff21a5527f756e3edfbf96
Hrittik Raushan <hrittikraushan9939@gmail.com>
2026-02-25 09:41:17 +0530

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-console.log("Hello World!");
-console.log("This is a JavaScript file.");
+// console.log("Hello World!");
+// console.log("This is a JavaScript file.");
+
+var num =10;
+console.log(num);
+console.log(typeof(num));
+

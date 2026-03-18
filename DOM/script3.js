dbccaecf2610998afe13e8505f38c98914a6d035
Hrittik Raushan <hrittikraushan9939@gmail.com>
2026-03-18 10:22:18 +0530

    events

---
+++ DOM/script3.js
@@ -0,0 +1,46 @@
+// const result=document.getElementById("hr");
+// console.log(result);
+
+// const result1=document.getElementById("p1");
+// console.log(result1);
+
+// const result2=document.getElementById("p2");
+// console.log(result2);
+
+
+// const result = document.getElementById("hr").getAttribute("class");
+// const x = document.getElementById("p1").innerHTML = result;
+
+
+
+function onClick(){
+    console.log("Clicked!")
+}
+
+function onMouseover(){
+    console.log("Mouse over!")
+}
+
+function onMouseout(){
+    console.log("Mouse out!")
+}
+
+function onMouseDown(){
+    console.log("Mouse down!")
+}
+
+function onMouseUp(){
+    console.log("Mouse up!")
+}
+
+function onMouseMove(){
+    console.log("Mouse move!")
+}
+
+function onDoubleClick(){
+    console.log("Double click!")
+}
+
+function myFunction(){
+    alert("Window Loaded Successfully!")
+}

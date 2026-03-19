6e65799033f99017490a7a8bc09fce3ceca596ba
Hrittik Raushan <hrittikraushan9939@gmail.com>
2026-03-19 12:15:30 +0530

    event

--- exp3/script.js
+++ exp3/script.js
@@ -20,3 +20,27 @@
 document.getElementById("bgColorBtn").onClick=function(){
     document.body.style.backgroundColor="#"+Math.floor(Math.random()*16777215).toString(16);
 };
+
+// incresase font size
+document.getElementById("fontSizeBtn").addEventListener("click", function(){
+    fontSize +=2;
+    paragraph.style.fontSize = fontSize + "px";
+});
+
+// show/hide paragraph
+document.getElementById("toggleBtn").addEventListener("click", function(){
+    if(paragraph.style.display === "none"){
+        paragraph.style.display = "block";
+    } else {
+        paragraph.style.display = "none";
+    }
+});
+
+// reset page
+document.getElementById("resetBtn").addEventListener("click", function(){
+    HTMLHeadingElement.innerHTML = "Welcome to JavaScript DOM Manipulation!";
+    document.body.style.backgroundColor = "#ffffff";
+    paragraph.style.fontSize = "16px";
+    paragraph.style.display = "block";
+    input.value = "";
+});

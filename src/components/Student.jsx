615fb689b13465279aecd2298c018721cafb8d16
Hrittik Raushan <hrittikraushan9939@gmail.com>
2026-04-16 12:17:05 +0530

    Exp5

---
+++ src/components/Student.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Student() {
+  return (
+    <div>
+      <Student name="Hritik" age={21} />
+      <Student name="Rahul" age={22} />
+      <Student name="Priya" age={20} />
+    </div>
+  )
+}
+
+export default Student
